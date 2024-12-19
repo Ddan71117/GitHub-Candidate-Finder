9d1dfc1fa55eb10ffbd532a861f0fabf8f513937
Ddan71117 <137979308+Ddan71117@users.noreply.github.com>
2024-12-19 12:47:23 -0700

    Completed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
+import { Outlet, useLocation } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <Nav />
+      <div className="d-flex flex-row justify-content-center mb-4">
+        {" "}
+        <Nav currentPage={location.pathname} />
+      </div>
       <main>
         <Outlet />
       </main>
--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+import { Candidate } from "../interfaces/Candidate.interface";
+import CandidateCard from "../components/CandidateCard";
+
 const SavedCandidates = () => {
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const storedCandidates = localStorage.getItem("savedCandidates");
+    if (storedCandidates) {
+      setSavedCandidates(JSON.parse(storedCandidates));
+    }
+  }, []);
+
+  const handleRemoveClick = (login: string) => {
+    const updatedCandidates = savedCandidates.filter(
+      (candidate) => candidate.login !== login
+    );
+    setSavedCandidates(updatedCandidates);
+
+    localStorage.setItem("savedCandidates", JSON.stringify(updatedCandidates));
+  };
+
   return (
     <>
-      <h1>Potential Candidates</h1>
+      <header className="text-center mb-4">
+        <h1>Saved Candidates</h1>
+      </header>
+
+      {savedCandidates.length === 0 ? (
+        <p>There are currently no candidates for viewing.</p>
+      ) : (
+        <div className="container">
+          <div className="row justify-content-evenly">
+            {savedCandidates.map((candidate) => (
+              <CandidateCard
+                candidate={candidate}
+                savedToPage={true}
+                handleRemoveClick={handleRemoveClick}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 };
